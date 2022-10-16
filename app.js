e1347b864914718a95979f3f1906d98e0a49011c
shamim 1845m <samimraj1845@gmail.com>
2022-10-16 12:45:39 +0600

    Cors added

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const cookieParser = require("cookie-parser");
-
+const cors = require("cors");
 const dotenv = require('dotenv');
 
 
 
 const app = express();
 /******* MiddleWare ********/
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 app.use(cookieParser());
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const app = require("./app")
 
 const dotenv = require('dotenv');
+
 const connectDatabase = require("./config/database");
 
 // Handling Uncaught Exception
